16fe6288294a5b939e8f5a547ecbf3cc1cd4a6a4
yechanhwangahah <yechanhwangahah@gmail.com>
2026-03-31 19:27:33 +0900

    rep02replace1

---
+++ src/Rep02Replace1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Rep02Replace1 {
+
+    static void main() {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a line of text.");
+
+        String str = s.nextLine();
+        System.out.println(str);
+
+
+        System.out.println("You enterd:" + str);
+
+
+
+
+
+    }
+}
